26a52384256d0bf46d8f722ac02e0f376796a4f2
unknown <jithinbp007@gmail.com>
2023-11-27 17:09:35 +0530

    added spacing

--- main.js
+++ main.js
@@ -3,6 +3,8 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const iconHam = document.querySelector(".icon-hamburger");
+const iconClose = document.querySelector(".icon-close");
 
 navToggle.addEventListener('click', (e) => {
   primaryNav.hasAttribute("data-visible")
@@ -10,6 +12,8 @@ navToggle.addEventListener('click', (e) => {
     : navToggle.setAttribute("aria-expanded", true);
   primaryNav.toggleAttribute("data-visible");
   primaryHeader.toggleAttribute("data-overlay");
+  iconHam.toggleAttribute("data-visible");
+  iconClose.toggleAttribute("data-visible");
 });
 
 
